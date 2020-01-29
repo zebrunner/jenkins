36e27137d164c5f0cc7dfa38a03f24f5d624c112
ilya-varchenya <ilya.varchenya@gmail.com>
2020-01-29 16:15:05 +0300

    add logs

--- resources/init.groovy.d/setup_ghprbhook_credentials.groovy
+++ resources/init.groovy.d/setup_ghprbhook_credentials.groovy
@@ -10,6 +10,9 @@ println "--> setting ghprbhook creds"
 
 auth.setAccessible(true)
 
+println "MEW_MEW_MEW_MEW_MEW"
+println GhprbGitHubAuth
+
 def githubAuth = new ArrayList<GhprbGitHubAuth>(1)
 
 println "MEW_MEW_MEW" + githubAuth
