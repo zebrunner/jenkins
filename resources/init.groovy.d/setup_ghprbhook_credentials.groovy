67fa14195d5b19577a5e3d6da232374c55635dd2
ilya-varchenya <ilya.varchenya@gmail.com>
2020-01-30 11:24:39 +0300

    comment adding ghprbhook creds

--- resources/init.groovy.d/setup_ghprbhook_credentials.groovy
+++ resources/init.groovy.d/setup_ghprbhook_credentials.groovy
@@ -1,25 +1,25 @@
-import jenkins.model.*
-import java.lang.reflect.Field
-import org.jenkinsci.plugins.ghprb.*
-
-def descriptor = Jenkins.instance.getDescriptorByType(org.jenkinsci.plugins.ghprb.GhprbTrigger.DescriptorImpl.class)
-
-Field auth = descriptor.class.getDeclaredField("githubAuth")
-
-println "--> setting ghprbhook creds"
-
-auth.setAccessible(true)
-
-//def githubAuth = new ArrayList<GhprbGitHubAuth>(1)
-
-//println "MEW_MEW_MEW" + githubAuth
-
-// serverAPIUrl, jenkinsUrl, credentialsId, description, id, secret
-githubAuth.add(new GhprbGitHubAuth("https://api.github.com", "", "ghprbhook-token", "GitHub Pull Request Builder token", "CHANGE_ME", "CHANGE_ME"))
-
-auth.set(descriptor, githubAuth)
-
-descriptor.save()
-
-return
-
+//import jenkins.model.*
+//import java.lang.reflect.Field
+//import org.jenkinsci.plugins.ghprb.*
+//
+//def descriptor = Jenkins.instance.getDescriptorByType(org.jenkinsci.plugins.ghprb.GhprbTrigger.DescriptorImpl.class)
+//
+//Field auth = descriptor.class.getDeclaredField("githubAuth")
+//
+//println "--> setting ghprbhook creds"
+//
+//auth.setAccessible(true)
+//
+////def githubAuth = new ArrayList<GhprbGitHubAuth>(1)
+//
+////println "MEW_MEW_MEW" + githubAuth
+//
+//// serverAPIUrl, jenkinsUrl, credentialsId, description, id, secret
+//githubAuth.add(new GhprbGitHubAuth("https://api.github.com", "", "ghprbhook-token", "GitHub Pull Request Builder token", "CHANGE_ME", "CHANGE_ME"))
+//
+//auth.set(descriptor, githubAuth)
+//
+//descriptor.save()
+//
+//return
+//
