6260af3de9145b8afa9619aa39dee2105a0a0ed8
ilya-varchenya <ilya.varchenya@gmail.com>
2020-04-07 12:12:58 +0300

    version settings #2

--- resources/init.groovy.d/configGradleAutoInstaller.groovy
+++ resources/init.groovy.d/configGradleAutoInstaller.groovy
@@ -7,7 +7,7 @@ import hudson.util.DescribableList;
 def gradleDesc = jenkins.model.Jenkins.instance.getExtensionList(hudson.tasks.gradleGradle)[0]
 
 def isp = new InstallSourceProperty()
-def autoInstaller = new hudson.plugins.gradle.GradleInstaller("Gradle 6.2.2")
+def autoInstaller = new hudson.plugins.gradle.GradleInstaller("6.2.2")
 isp.installers.add(autoInstaller)
 
 def proplist = new DescribableList<ToolProperty<?>, ToolPropertyDescriptor>()
