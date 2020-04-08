2394504a5b6afed16d103077ca250735c07d9f8a
ilya-varchenya <ilya.varchenya@gmail.com>
2020-04-08 11:02:40 +0300

    change instance name

--- resources/init.groovy.d/configGradleAutoInstaller.groovy
+++ resources/init.groovy.d/configGradleAutoInstaller.groovy
@@ -7,12 +7,13 @@ import hudson.util.DescribableList;
 
 def gradleDesc = jenkins.model.Jenkins.instance.getExtensionList(hudson.plugins.gradle.Gradle)[0]
 
-def isp = new InstallSourceProperty()
+def installSourceProperty = new InstallSourceProperty()
+
 def autoInstaller = new GradleInstaller("6.2.2")
-isp.installers.add(autoInstaller)
+installSourceProperty.installers.add(autoInstaller)
 
 def proplist = new DescribableList<ToolProperty<?>, ToolPropertyDescriptor>()
-proplist.add(isp)
+proplist.add(installSourceProperty)
 
 def installation = new GradleInstallation("G6", "", proplist)
 
