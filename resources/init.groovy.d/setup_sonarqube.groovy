c6c63bcd9c239bdc42a56e9029c2787817400af4
sandinosanchez <sandino320@gmail.com>
2020-04-28 12:15:15 -0300

    using different constructor for SonarInstallation

--- resources/init.groovy.d/setup_sonarqube.groovy
+++ resources/init.groovy.d/setup_sonarqube.groovy
@@ -9,9 +9,6 @@ def env = System.getenv()
 // Variables
 def sonarName = env['SONAR_NAME']
 def sonarUrl = env['SONAR_URL']
-def sonarUser = env['SONAR_USER']
-def sonarPassword = env['SONAR_PASS']
-def sonarRunnerName = env['SONAR_RUNNER_NAME']
 def sonarRunnerVersion = env['SONAR_RUNNER_VERSION']
 
 // Constants
@@ -22,7 +19,7 @@ Thread.start {
       // source: https://github.com/ridakk/jenkins/blob/master/groovy-scripts/setup-sonarqube-plugin.groovy
       println "--> setting SonarQube plugin"
       def SonarGlobalConfiguration sonarConfig = instance.getDescriptor(SonarGlobalConfiguration.class)
-      def sonar = new SonarInstallation(sonarName, sonarUrl, sonarName + sonarPassword, '', '', new TriggersConfig(), '')
+      def sonar = new SonarInstallation(sonarName, sonarUrl, "sonarqube-token", null, '', '', '', '', new TriggersConfig())
 
       def sonarInstallations = sonarConfig.getInstallations()
       def sonarInstExist = false
