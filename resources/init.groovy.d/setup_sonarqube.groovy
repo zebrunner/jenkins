cfbe721371f6bedd57253e78e2d61a606f6774bf
sandinosanchez <sandino320@gmail.com>
2020-05-07 05:06:06 -0300

    Added config for sonar token credentrial and attached it to sonar plugin config

--- resources/init.groovy.d/setup_sonarqube.groovy
+++ resources/init.groovy.d/setup_sonarqube.groovy
@@ -1,8 +1,13 @@
 import hudson.model.*
 import jenkins.model.*
-import hudson.plugins.sonar.*
-import hudson.plugins.sonar.model.TriggersConfig
 import hudson.tools.*
+import hudson.util.Secret
+import hudson.plugins.sonar.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import hudson.plugins.sonar.model.TriggersConfig
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
 
 def env = System.getenv()
 
@@ -14,13 +19,27 @@ def sonarRunnerName = env['SONAR_RUNNER_NAME']
 
 // Constants
 def instance = Jenkins.getInstance()
+def global_domain = Domain.global()
+
+def id = "sonar-token"
+def desc = "sonar-admin"
+def token = env['SONAR_TOKEN']
+
+def credentialsStore = instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+def avilableCredentials = CredentialsProvider.lookupCredentials(StringCredentialsImpl.class, instance, hudson.security.ACL.SYSTEM)
+def tokenExists = CredentialsMatchers.firstOrNull(avilableCredentials, CredentialsMatchers.withId(id))
 
 Thread.start {
+
+      if (!tokenExists) {
+        def sonarQubeToken = new StringCredentialsImpl(CredentialsScope.GLOBAL, id, desc, Secret.fromString(token))
+        credentialsStore.addCredentials(global_domain, sonarQubeToken)
+      }
       // SonarQube plugin config
       // source: https://github.com/ridakk/jenkins/blob/master/groovy-scripts/setup-sonarqube-plugin.groovy
-      println "--> setting SonarQube plugin"
       def SonarGlobalConfiguration sonarConfig = instance.getDescriptor(SonarGlobalConfiguration.class)
-      def sonar = new SonarInstallation(sonarName, sonarUrl, "sonarqube-token", null, '', '', '', '', new TriggersConfig())
+      def sonar = new SonarInstallation(sonarName, sonarUrl, id, null, '', '', '', '', new TriggersConfig())
 
       def sonarInstallations = sonarConfig.getInstallations()
       def sonarInstExist = false
@@ -32,12 +51,12 @@ Thread.start {
         }
       }
       if (!sonarInstExist) {
+        println "--> setting SonarQube plugin"
         sonarInstallations += sonar
         sonarConfig.setInstallations((SonarInstallation[]) sonarInstallations)
         sonarConfig.save()
       }
 
-      println "--> setting up SonarRunner"
       def runnerInstDesc = instance.getDescriptor("hudson.plugins.sonar.SonarRunnerInstallation")
       def runnerInstaller = new SonarRunnerInstaller(sonarRunnerVersion)
       def installSourceProperty = new InstallSourceProperty([runnerInstaller])
@@ -54,6 +73,7 @@ Thread.start {
       }
 
       if (!runnerInstExists) {
+        println "--> setting up SonarRunner"
         runnerInstallations += runnerInst
         runnerInstDesc.setInstallations((SonarRunnerInstallation[]) runnerInstallations)
         runnerInstDesc.save()
