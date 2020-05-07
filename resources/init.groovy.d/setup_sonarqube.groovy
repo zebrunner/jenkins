fcbd98e62df108a08f5635791c8d7833fabe7b2b
sandinosanchez <sandino320@gmail.com>
2020-05-07 08:00:15 -0300

    removing autmated config for sonar user/token

--- resources/init.groovy.d/setup_sonarqube.groovy
+++ resources/init.groovy.d/setup_sonarqube.groovy
@@ -1,13 +1,8 @@
 import hudson.model.*
 import jenkins.model.*
 import hudson.tools.*
-import hudson.util.Secret
 import hudson.plugins.sonar.*
-import com.cloudbees.plugins.credentials.*
-import com.cloudbees.plugins.credentials.common.*
-import com.cloudbees.plugins.credentials.domains.*
 import hudson.plugins.sonar.model.TriggersConfig
-import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
 
 def env = System.getenv()
 
@@ -19,27 +14,12 @@ def sonarRunnerName = env['SONAR_RUNNER_NAME']
 
 // Constants
 def instance = Jenkins.getInstance()
-def global_domain = Domain.global()
-
-def id = "sonar-token"
-def desc = "sonar-admin"
-def token = env['SONAR_TOKEN']
-
-def credentialsStore = instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
-
-def avilableCredentials = CredentialsProvider.lookupCredentials(StringCredentialsImpl.class, instance, hudson.security.ACL.SYSTEM)
-def tokenExists = CredentialsMatchers.firstOrNull(avilableCredentials, CredentialsMatchers.withId(id))
 
 Thread.start {
-
-      if (!tokenExists) {
-        def sonarQubeToken = new StringCredentialsImpl(CredentialsScope.GLOBAL, id, desc, Secret.fromString(token))
-        credentialsStore.addCredentials(global_domain, sonarQubeToken)
-      }
       // SonarQube plugin config
       // source: https://github.com/ridakk/jenkins/blob/master/groovy-scripts/setup-sonarqube-plugin.groovy
       def SonarGlobalConfiguration sonarConfig = instance.getDescriptor(SonarGlobalConfiguration.class)
-      def sonar = new SonarInstallation(sonarName, sonarUrl, id, null, '', '', '', '', new TriggersConfig())
+      def sonar = new SonarInstallation(sonarName, sonarUrl, "sonar-token", null, '', '', '', '', new TriggersConfig())
 
       def sonarInstallations = sonarConfig.getInstallations()
       def sonarInstExist = false
