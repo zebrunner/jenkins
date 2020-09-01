5e87b4fb66963bb25c404a2a173c65e1cde1086b
ilya-varchenya <ilya.varchenya@gmail.com>
2020-09-01 13:08:25 +0300

    delete empty line

--- resources/init.groovy.d/global-args-security.groovy
+++ resources/init.groovy.d/global-args-security.groovy
@@ -115,7 +115,6 @@ Thread.start {
         Field auth = descriptor.class.getDeclaredField("githubAuth")
         auth.setAccessible(true)
         def githubAuth = new ArrayList<GhprbGitHubAuth>(1)
-        
         Secret secret = Secret.fromString('')
         githubAuth.add(new GhprbGitHubAuth("https://api.github.com", "", id, description, username, secret))
         auth.set(descriptor, githubAuth)
