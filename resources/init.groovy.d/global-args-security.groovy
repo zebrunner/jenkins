513f329ed15beabf45f855a52524e92182c51a35
ilya-varchenya <ilya.varchenya@gmail.com>
2020-09-02 11:46:11 +0300

    set permissions

--- resources/init.groovy.d/global-args-security.groovy
+++ resources/init.groovy.d/global-args-security.groovy
@@ -1,4 +1,5 @@
 import hudson.model.*;
+import hudson.security.GlobalMatrixAuthorizationStrategy;
 import jenkins.model.*;
 import hudson.security.*;
 import com.cloudbees.plugins.credentials.*;
@@ -119,7 +120,7 @@ Thread.start {
         println envVars.get("JENKINS_SECURITY_INITIALIZED")
 
         println "--> setting ghprhook creds"
-
+        GlobalMatrixAuthorizationStrategy.dangerousPermissions = true
         credentialsStore.addCredentials(global_domain, ghprbhookCredentials)
         def descriptor = Jenkins.instance.getDescriptorByType(org.jenkinsci.plugins.ghprb.GhprbTrigger.DescriptorImpl.class)
         Field auth = descriptor.class.getDeclaredField("githubAuth")
