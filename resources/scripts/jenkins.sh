df37365a7e37f7382f3e248b2ccc688d5ec4419a
azarouski <azarouski@zebrunner.com>
2023-10-13 19:00:20 +0000

    plugins installation logic was edited

--- resources/scripts/jenkins.sh
+++ resources/scripts/jenkins.sh
@@ -1,5 +1,18 @@
 #! /bin/bash -e
 
+# Check if the plugins dir is empty and if so install plugins
+echo "Check if plugins dir is empty:"
+if [ ! "$(ls -A /var/jenkins_home/plugins)" ]; then
+  echo "- plugins dir is empty, installing plugins"
+  echo "plugins to install: "
+  echo "$( cat /var/jenkins_home/plugins.txt )"
+  /bin/jenkins-plugin-cli --plugin-file /usr/share/jenkins/ref/plugins.txt
+else
+  echo "- plugins dir is not empty, no actions needed"
+  echo "installed plugins:"
+  echo "$(ls -d /var/jenkins_home/plugins/*/)"
+fi
+
 # upload extra plugins at run-time during first initialization only
 if [[ -f /var/jenkins_home/extra_plugins.txt.installed ]]; then
   echo "Zebrunner Jenkins extra-plugins already installed."
